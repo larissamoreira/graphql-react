eb31eaade98e552ae989c2f2987e4ada26c0fbf8
Larissa Moreira <larissamgusmao@gmail.com>
2019-06-22 16:36:48 -0400

    Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -41,21 +42,42 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc, _id: event.id };
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
         },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+        createEvent: args => {
+            const event = new Event({
                 title: args.EventInput.title,
                 description: args.EventInput.description,
                 price: +args.EventInput.price,
-                date: args.EventInput.date
-            }
-            events.push(event);
-            return event;
+                date: new Date(args.EventInput.date)
+            })
+            return event
+                .save()
+                .then(result => {
+                    console.log(result);
+                    return { ...result._doc, _id: event.id };
+                })
+                .catch(err => {
+                    console.log(err)
+                    throw err;
+                });
         }
     },
     graphiql: true
 }));
 
-app.listen(3000);
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-q1b2l.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+).then(() => {
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+})
